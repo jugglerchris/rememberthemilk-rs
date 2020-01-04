593611e79310925019c92c7162ab2002c80f99f9
Chris Emerson <github@mail.nosreme.org>
2020-01-04 23:34:43 +0000

    Trivial echo example.

---
+++ examples/echo.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error>
+{
+    let api_key = env::args().skip(1).next().unwrap();
+    let body = reqwest::get(&format!("https://api.rememberthemilk.com/services/rest/?method=rtm.test.echo&api_key={}&name=foo", api_key))
+                        .await?
+                        .text()
+                        .await?;
+    println!("Body={}", body);
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+pub struct API {
+    api_key: String,
+}
+
+impl API {
+    pub fn new() -> API {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
