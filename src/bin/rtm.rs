3227900a92b7445dc0ffc0470111f53294fbb39f
Chris Emerson <github@mail.nosreme.org>
2020-01-26 07:58:52 +0000

    Add structopt for command line parsing.

--- src/bin/rtm.rs
+++ src/bin/rtm.rs
@@ -1,3 +1,22 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt,Debug)]
+enum Command {
+    /// Dummy command
+    Dummy,
+}
+
+#[derive(StructOpt,Debug)]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
 fn main() {
-    println!("rtm");
+    let opt = Opt::from_args();
+    match opt.cmd {
+        Command::Dummy => {
+            println!("Dummy");
+        }
+    }
 }
